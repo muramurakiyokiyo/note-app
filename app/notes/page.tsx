c5b2e25297ba5bdf9e1ee5c27f805de1b2a9101e
muramurakiyokiyo <kiiyoichi.murata@gmail.com>
2023-11-05 19:55:28 +0900

    zodを使ってtypescriptの型を定義してみよう

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,26 +1,32 @@
 import { getNotesList } from '@/lib/client';
 import Link from 'next/link' //コレがないとエラー。動画では不要
+import { Note } from './type';
+
+type NoteProps = {
+    note: Note;
+};
 
 export default async function Page(){
 
     const notelist = await getNotesList();
-    console.log(notelist.contents);
 
     return (
         <div className='grid sm:grid-cols-2 xl:grid-cols-3 gap-8 sm:gap-y-10 mt-10'>
-            <NoteItem/>
-            <NoteItem/>
-            <NoteItem/>
+            {
+                notelist.contents.map((note)=>(
+                    <NoteItem key={note.id} note={note} />    
+                ))
+            }
         </div>
     );
 }
 
-const NoteItem = ()=>{
+const NoteItem = ({note}: NoteProps)=>{
     return(
         <div className="bg-gray-100 rounded-lg p-5">
             <Link href={`/notes/1`}>
-                <h3 className='text-purple-500 hover:text-purple-700 text-lg md:text-x1 font-semibold mb-3 underline'>初めてのノートです</h3>
-                ここに内容詳細
+                <h3 className='text-purple-500 hover:text-purple-700 text-lg md:text-x1 font-semibold mb-3 underline'>{note.title}</h3>
+                {note.content}
             </Link>
         </div>
     );

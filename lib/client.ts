0518d95814df17033b05abd570afbeddb361e228
muramurakiyokiyo <kiiyoichi.murata@gmail.com>
2023-11-08 01:58:02 +0900

    キャッシュの無効化をいったんロールバック

--- lib/client.ts
+++ lib/client.ts
@@ -8,10 +8,7 @@ export const client = createClient({
 
 export const getNotesList = async ()=>{
     const notes = await client.getList<Note>({
-        customRequestInit: {
-            cache: "no-store", // キャッシュを利用せずに常に新しいデータを取得する
-        },
-        endpoint: "note_app",
+          endpoint: "note_app",
     });
     return notes;
 };
